2b0df08546c56b60f9645610e7d1e03992215397
Akash Gupta <deepak.gpta96@gmail.com>
2026-01-26 20:22:22 +0530

    chatpage codebase setup

---
+++ frontend/src/components/NoConversationPlace.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NoConversationPlace = () => {
+  return (
+    <div>NoConversationPlace</div>
+  )
+}
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { useAuthStore } from '../store/useAuthStore.js'
+import { ProfileHeader } from '../components/ProfileHeader.jsx';
+import {ActiveTabSwitch} from '../components/ActiveTabSwitch.jsx'
+import {ChatList} from '../components/ChatList.jsx'
+import {ChatContainer} from '../components/ChatContainer.jsx'
+import {NoConversationPlace} from '../components/NoConversationPlace.jsx'
 
 export const ChatPage = () => {
   const {logout}=useAuthStore();
   return (
-    <div>ChatPage
-      <button onClick={logout}>logout</button>
-    </div>
-
+    <>
+      <div id='chatWindow' className='container m-5 p-3 row'>
+          <div className="col-3 p-3 lh-lg border-end ">
+            <ProfileHeader/>
+            <ActiveTabSwitch/>
+            <div>
+              <ChatList/>
+            </div>
+          </div>
+          <div className="col-9 p-3 ">
+            <NoConversationPlace/>
+          </div>
+      </div>
+    </>
   )
 }
     
